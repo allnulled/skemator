e1dc73a807de3475b1612fff2714c30c2761e479
allnulled <todosanulados@gmail.com>
2019-09-18 10:12:43 +0200

    Separating environments (browser and node)

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
-if(typeof global === "object" && typeof process !== "undefined") {
-	module.exports = require("./skemator.js");
-} else 
-if(typeof window === "object" && typeof navigator === "object") {
+if(typeof window === "object" && typeof navigator === "object" && typeof HTMLElement === "function") {
 	window.Skemator = require("./skemator.browser.js");
+} else if(typeof global === "object" && typeof process !== "undefined") {
+	module.exports = require("./skemator.js");
 }
